6fbb178fc52a6ac452a4b7a997ac4d12bd804ab1
Luminoso01 <novoaavila16@gmail.com>
2024-08-30 21:21:36 -0500

    enhance validate-schema middleware

--- backend/src/middlewares/validate-schema.js
+++ backend/src/middlewares/validate-schema.js
@@ -1,20 +1,28 @@
 import { ZodError } from 'zod'
+import fs from 'fs'
 
 export const validate = (schemas) => async (req, res, next) => {
-  for (const [key, schema] of Object.entries(schemas)) {
-    try {
-      const result = schema.parse(req[key])
-      req[key] = result
-      next()
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const details = error.issues.map((err) => {
-          return { label: err.path[0], message: err.message }
-        })
-
-        return res.status(400).json({ error: 'Invalid data', details })
-      }
-      return res.status(400).json({ error: 'Invalid data', details: error })
+  try {
+    for (const [key, schema] of Object.entries(schemas)) {
+      req[key] = schema.parse(req[key])
+      console.log(req[key])
     }
+    next()
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const details = error.issues.map((err) => ({
+        label: err.path[0],
+        message: err.message
+      }))
+      const isFileError = details.some(({ label }) => ['size', 'mimetype'].includes(label))
+      if (isFileError) {
+        const filePath = req.file.path
+        fs.unlinkSync(filePath)
+      }
+
+      return res.status(400).json({ error: 'Invalid data', details })
+    }
+
+    return res.status(400).json({ error: 'Invalid data', details: error.message })
   }
 }
