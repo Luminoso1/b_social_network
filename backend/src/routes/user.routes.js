98554d26c63fda190a108892da685e4fe6a3e37e
Luminoso01 <novoaavila16@gmail.com>
2024-09-05 14:23:21 -0500

    user route fixed

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,5 @@
 import { Router } from 'express'
-<<<<<<< HEAD
 import { profile, userListWithoutDep, updateUser, uploadImage, avatar, getUser } from '../controllers/user.js'
-=======
-import { register, login, profile, userListWithoutDep, updateUser, logout, uploadImage, avatar, getUser } from '../controllers/user.js'
->>>>>>> 3f18b8c4a8aec87d7c5017c39bcc9a96db323fc6
 import { validate } from '../middlewares/validate-schema.js'
 import { userUpdateSchema } from '../models/zod/user.js'
 import { fileSchema } from '../models/zod/file.js'
@@ -14,19 +10,9 @@ const router = Router()
 
 const upload = defineStorage('avatars')
 
-<<<<<<< HEAD
-router.get('/profile', validateAuthCookie, profile)
-=======
-router.post('/register', validate({ body: userSchema }), register)
-
-router.post('/login', validate({ body: loginSchema }), login)
-
-router.get('/logout', logout)
-
 router.get('/profile', validateAuthCookie, profile)
 
-router.get('/profile/:id', validateAuthCookie, getUser)
->>>>>>> 3f18b8c4a8aec87d7c5017c39bcc9a96db323fc6
+router.get('/users/:id', validateAuthCookie, getUser)
 
 router.get('/users', validateAuthCookie, userListWithoutDep)
 
